50f43e9d1a7f15595679365d74171f3cfd040892
unknown <edwardhornig@gmail.com>
2015-12-02 18:02:02 -0500

    work

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
 
   var rect_array = generateRects(10);
 
+  rect_array = packRects(rect_array);
+
+  rect_array = convertCoords(rect_array, $(window).width(), $(window).height());
+
+  drawPixi(rect_array);
+
   console.log(rect_array);
 
-  drawPixi();
-
 });
-
 
 generateRects = function(n) {
   var rect_array = [];
@@ -21,13 +24,34 @@ generateRects = function(n) {
   return rect_array;
 }
 
-drawPixi = function() {
+
+packRects = function(rect_array) {
+  for (var i=0; i<rect_array.length; i++) {
+    if (i == 0) {
+      rect_array[i].x = 0;
+      rect_array[i].y = 0;
+    }
+  }
+  return rect_array;
+}
+
+convertCoords = function(rect_array, renderer_width, renderer_height) {
+  var scale = 1000;
+  for (var i=0; i<rect_array.length; i++) {
+    rect_array[i].x = (rect_array[i].x / scale) * renderer_width + renderer_width / 2;
+    rect_array[i].y = (rect_array[i].y / scale) * renderer_height + renderer_height / 2 - rect_array[i].height;
+  }
+  return rect_array;
+}
+
+drawPixi = function(packed_array) {
   var renderer = new PIXI.WebGLRenderer($(window).width(), $(window).height());
   document.body.appendChild(renderer.view);
   var stage = new PIXI.Container();
   var graphics = new PIXI.Graphics();
 
-  graphics.lineStyle(5, 0xFFFF00);
+  graphics.lineStyle(1, 0xFFFF00);
+  graphics.beginFill(0xFFFF00);
 
   var x_origin = renderer.width / 2;
   var y_origin = renderer.height / 2;
@@ -37,7 +61,7 @@ drawPixi = function() {
   graphics.moveTo(0, y_origin);
   graphics.lineTo(renderer.width, y_origin);
 
-  graphics.drawRect(0, 0, 200, 100);
+  graphics.drawRect(packed_array[0].x, packed_array[0].y, packed_array[0].width, packed_array[0].height);
   stage.addChild(graphics);
   renderer.render(stage);
 }
