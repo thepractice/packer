4c041cf2bd8820216dcd8d778002b800eed043dc
unknown <edwardhornig@gmail.com>
2015-12-15 16:11:06 -0500

    refactor

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+var corners = [];
+var rect_array = [];
+var vertex_array = [];
+var usedCorners = [];
+
 $(document).ready(function() {
 
-  var dimensions_array = generateRects(10);
+  var dimensions_array = generateRects(30);
 
-  packed_array = packRects(dimensions_array);
+  packed_array = packRectsWrapper(dimensions_array);
 
   conv_array = convertCoords(packed_array, $(window).width(), $(window).height());
 
@@ -24,6 +29,11 @@ rectangle = function(x, y, width, height) {
   this.v4 = new vertex(x, y - height);
 }
 
+dimension = function(width, height) {
+  this.width = width;
+  this.height = height;
+}
+
 vertex = function(x, y) {
   this.x = x;
   this.y = y;
@@ -34,198 +44,82 @@ generateRects = function(n) {
   for (var i=0; i<n; i++) {
     var w = 40+ Math.round(Math.random() * 100);
     var h = 40+ Math.round(Math.random() * 100);
-    //var rect = {width: w, height: h};
-    //var rect = new rectangle(w, h);
-    dimensions_array.push([w, h]);
+    var d = new dimension(w, h);
+    dimensions_array.push(d);
   }
   return dimensions_array;
 }
 
+firstTwoRect = function(dimensions_array) {
 
+  var width1 = dimensions_array[0].width;
+  var height1 = dimensions_array[0].height;
+  var x1 = - width1 / 2;
+  var y1 = height1 / 2;
+  var r1 = new rectangle(x1, y1, width1, height1);
+  var width2 = dimensions_array[1].width;
+  var height2 = dimensions_array[1].height;
+  var x2 = x1;
+  var y2 = y1 + height2;
+  var r2 = new rectangle(x2, y2, width2, height2);
+  rect_array.push(r1, r2);
+  vertex_array.push(r1.v1, r1.v2, r1.v3, r1.v4, r2.v1, r2.v2, r2.v3, r2.v4);
 
+  var sharedVertex = r1.v1;
+  corners = findCorners(sharedVertex, vertex_array, usedCorners);
+  usedCorners.push(sharedVertex);
+}
 
-packRects = function(dimensions_array) {
-  //var rect_array = jQuery.extend(true, [], rect_array_in);
-  var rect_array = [];
-  var vertex_array = [];
+packRectsWrapper = function(dimensions_array) {
+
+  firstTwoRect(dimensions_array);
+
+  dimensions_array = dimensions_array.slice(2);
+
   for (var i=0; i<dimensions_array.length; i++) {
-    var width = dimensions_array[i][0];
-    var height = dimensions_array[i][1];
+    var width = dimensions_array[i].width;
+    var height = dimensions_array[i].height;
 
-    if (i == 0) {
-      var x = - width / 2;
-      var y = height / 2;
-      var r = new rectangle(x, y, width, height);
-      rect_array.push(r);
+    corners.sort(function(a, b) {
+      return a.distance - b.distance;
+    });
 
+    var indivRectInfo = packIndivRect(width, height);
+    var r = indivRectInfo.rectangle;
+    var corner = indivRectInfo.corner;
 
-/*
-      r.x = - r.width / 2;
-      r.y = r.height / 2;
-      r.v1 = new vertex(r.x, r.y);
-      r.v2 = new vertex(r.x + r.width, r.y);
-      r.v3 = new vertex(r.x + r.width, r.y - r.height);
-      r.v4 = new vertex(r.x, r.y - r.height);
-*/
-      vertex_array.push(r.v1, r.v2, r.v3, r.v4);
+    usedCorners.push(corner);
+    vertex_array.push(r.v1, r.v2, r.v3, r.v4);
+    rect_array.push(r);
+    var newCorners = findCorners(corner, vertex_array, usedCorners);
+    for (var j=0; j<newCorners.length; j++) {
+      corners.push(newCorners[j]);
     }
-    if (i == 1) {
-
-      x = rect_array[0].x;
-      y = rect_array[0].y + height;
-      r = new rectangle(x, y, width, height);
-      rect_array.push(r);
-/*
-      r.x = rect_array[0].x;
-      r.y = rect_array[0].y + r.height;
-      r.v1 = new vertex(r.x, r.y);
-      r.v2 = new vertex(r.x + r.width, r.y);
-      r.v3 = new vertex(r.x + r.width, r.y - r.height);
-      r.v4 = new vertex(r.x, r.y - r.height);
-*/
-      var sharedVertex = findSharedVertex(rect_array, vertex_array);
-
-      vertex_array.push(r.v1, r.v2, r.v3, r.v4);
-
-      usedCorners = [];
-      // start array of available corners
-      corners = [];
-      corners = findCorners(sharedVertex, vertex_array, usedCorners);
-      // start array of used corners
-      usedCorners.push(sharedVertex);
-
-    }
-
-    if (i >= 2) {
-
-      corners.sort(function(a, b) {
-        return a.distance - b.distance;
-      });
-      overlap = true;
-
-      var counter = 0;
-      for (var j=0; j<corners.length; j++) {
-        var nearestCorner = corners[j];
-       // counter++;
-
-        //var nearestCorner = findClosestCorner(corners);
-
-        // Try quadrant 1 convexity
-        x = nearestCorner.x;
-        y = nearestCorner.y + height;
-        r = new rectangle(x, y, width, height);
-        var overlap = checkOverlap(r, rect_array);
-        if (!overlap) {
-          break;
-        } else {
-          // Try quadrant 2
-          x = nearestCorner.x - width;
-          y = nearestCorner.y + height;
-          r = new rectangle(x, y, width, height);
-          overlap = checkOverlap(r, rect_array);
-          if (!overlap) {
-            break;
-          } else {
-            // try quadrant 3
-            x = nearestCorner.x - width;
-            y = nearestCorner.y;
-            r = new rectangle(x, y, width, height);
-            overlap = checkOverlap(r, rect_array);
-            if (!overlap) {
-              break;
-            } else {
-              // try quadrant 4
-              x = nearestCorner.x;
-              y = nearestCorner.y;
-              r = new rectangle(x, y, width, height);
-              overlap = checkOverlap(r, rect_array);
-
-              if (!overlap) {
-
-
-                break;
-              } else {
-                if (j == corners.length - 1) {
-                  alert('failure');
-                }
-
-              }
-            }
-          }
-        }
-
-
-      }
-
-
-
-
-
-      rect_array.push(r);
-
-      // remove this corner from array of available corners
-      var index = corners.indexOf(nearestCorner);
-
-      console.log('spliced');
-      console.log(corners[index]);
-     // corners.splice(index, 1);
-      console.log('corners after splice');
-      console.log(corners);
-
-
-      // add this corner to array of used corners
-      usedCorners.push(nearestCorner);
-
-
-      sharedVertex = nearestCorner;
-      vertex_array.push(r.v1, r.v2, r.v3, r.v4);
-      var newCorners = findCorners(sharedVertex, vertex_array, usedCorners);
-
-      console.log('pre push');
-      console.log(corners);
-      for (var j=0; j<newCorners.length; j++) {
-
-        corners.push(newCorners[j]);
-        console.log('pushed');
-        console.log(newCorners[j]);
-      }
-
-
-    }
-
-/*
-    if (i == 3) {
-      //console.log(corners);
-      nearestCorner = findClosestCorner(corners);
-     // console.log(nearestCorner);
-
-      // remove this corner from array of available corners
-      var index = corners.indexOf(nearestCorner);
-      corners.splice(index, 1);
-
-      // add this corner to array of used corners
-      usedCorners.push(nearestCorner);
-
-      // Try quadrant 1 convexity
-      r.x = nearestCorner.x;
-      r.y = nearestCorner.y + r.height;
-      r.x2 = r.x + r.width;
-      r.y2 = r.y - r.height;
-
-      r.v1 = new vertex(r.x, r.y);
-      r.v2 = new vertex(r.x + r.width, r.y);
-      r.v3 = new vertex(r.x + r.width, r.y - r.height);
-      r.v4 = new vertex(r.x, r.y - r.height);
-
-
-      var overlap = checkOverlap(r, rect_array.slice(0, i));
-
-    }
-    */
-
   }
 
   return rect_array;
+
+}
+
+packIndivRect = function(width, height) {
+  console.log('corners');
+  console.log(corners);
+
+  for (var j=0; j<corners.length; j++) {
+    var corner = corners[j];
+    var quadrants = [[corner.x, corner.y + height], [corner.x - width, corner.y + height], [corner.x - width, corner.y], [corner.x, corner.y]];
+
+    for (var k=0; k<quadrants.length; k++) {
+      var x = quadrants[k][0];
+      var y = quadrants[k][1];
+      var r = new rectangle(x, y, width, height);
+      var overlap = checkOverlap(r, rect_array);
+      if (!overlap) {
+        return {rectangle: r, corner: corner};
+      }
+    }
+  }
+
 }
 
 checkOverlap = function(rect, rect_array) {
@@ -241,29 +135,22 @@ checkOverlap = function(rect, rect_array) {
 
 }
 
-findSharedVertex = function(rect_array, vertex_array) {
-  var lastRect = rect_array[rect_array.length - 1];
-  var newVertices = [lastRect.v1, lastRect.v2, lastRect.v3, lastRect.v4];
-  // For each new vertex, See if vertex already exists
-  for (var i=0; i<newVertices.length; i++) {
-    var commonVertex = vertex_array.filter(function(obj) {
-      if (obj.x == newVertices[i].x) {
-        if (obj.y == newVertices[i].y) {
-          return true;
-        }
-      }
-    });
-  }
-  return commonVertex[0];
-}
-
 findCorners = function(sharedVertex, vertex_array, usedCorners) {
 
   new_corners = [];
   var commonX = vertex_array.filter(function(obj) {
     if (obj.x == sharedVertex.x) {
       if (obj.y != sharedVertex.y) {
-        return true;
+        var used = false;
+        for (var i=0; i<usedCorners.length; i++) {
+          if (obj.x == usedCorners[i].x && obj.y == usedCorners[i].y) {
+            var used = true;
+            break;
+          }
+        }
+        if (!used) {
+          return true;
+        }
       }
     }
   });
@@ -336,27 +223,6 @@ getDistance = function(vertex) {
   return Math.pow(Math.pow(vertex.x, 2) + Math.pow(vertex.y, 2), 0.5);
 }
 
-findClosestCorner = function(vertex_array) {
-  var min = 99999999;
-  var closest = {};
-  for (var i=0; i<vertex_array.length; i++) {
-    if (vertex_array[i].distance < min) {
-      min = vertex_array[i].distance;
-      closest = vertex_array[i];
-    }
-  }
-  return closest;
-}
-
-sortByDistance = function(vertex_array) {
-  vertex_array.sort(function(a, b) {
-    if(a.distance > b.distance) return -1;
-    if(a.distance < b.distance) return 1;
-    return 0;
-  });
-}
-
-
 convertCoords = function(rect_array_in, renderer_width, renderer_height) {
   var rect_array = jQuery.extend(true, [], rect_array_in);
   var scale = 0.5;
@@ -406,8 +272,7 @@ drawPixi = function(rect_array) {
   //  graphics.drawCircle(r.x, r.y, 6);
   }
 
-  console.log('corners final');
-  console.log(corners);
+/*
   for (var j=0; j<corners.length; j++) {
     graphics.beginFill(0x5D0776);
     var converted = convertVertex(corners[j].x, corners[j].y, renderer.width, renderer.height);
@@ -424,7 +289,7 @@ drawPixi = function(rect_array) {
   //  graphics.drawCircle(converted[0], converted[1], 6);
 
   }
-
+*/
 
   //graphics.drawRect(rect_array[0].x, rect_array[0].y, rect_array[0].width, rect_array[0].height);
 
